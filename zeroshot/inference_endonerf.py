d03fc26975510815bd72dce3e225e7b0a5cf449d
Sissi <670408085@qq.com>
2025-03-31 18:38:39 +0800

    git commit -m "add scale and shift invariant function"

--- zeroshot/inference_endonerf.py
+++ zeroshot/inference_endonerf.py
@@ -2,7 +2,7 @@
 import argparse
 import sys
 import os
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../dust3r')))
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
