0215a1e41039d22db28bf6abd77ee73e2321d45c
Sissi <670408085@qq.com>
2025-05-10 11:31:18 +0800

    git commit -m "unfreeze head and decoder"

---
+++ zeroshot/inference_endo.sh
@@ -0,0 +1,22 @@
+Export CUDA_VISIBLE_DEVICES=0
+
+python ./zeroshot/inference_endoscope_testset.py \
+--base-dir /data_new/luxiaoxi/code_proj/MedicalMast3R \
+--input-dir /data_new/luxiaoxi/dataset/medical_depth/SCARED_keyframe_nearest_256 \
+--output-dir /data_new/luxiaoxi/dataset/medical_depth_output/endoscope_mast3r_unfreeze_decoder_encoder_0510 \
+--data-name scared \
+--model-name checkpoints/together_unfreeze_head_decoder_0510/checkpoint-best.pth
+
+python ./zeroshot/inference_endoscope_testset.py \
+--base-dir /data_new/luxiaoxi/code_proj/MedicalMast3R \
+--input-dir /data_new/luxiaoxi/dataset/medical_depth/EndoAbs_preprocessed_nearest \
+--output-dir /data_new/luxiaoxi/dataset/medical_depth_output/endoscope_mast3r_unfreeze_decoder_encoder_0510 \
+--data-name abs \
+--model-name checkpoints/together_unfreeze_head_decoder_0510/checkpoint-best.pth
+
+python ./zeroshot/inference_endoscope_testset.py \
+--base-dir /data_new/luxiaoxi/code_proj/MedicalMast3R \
+--input-dir /data_new/luxiaoxi/dataset/medical_depth/SERV-CT_preprocessed \
+--output-dir /data_new/luxiaoxi/dataset/medical_depth_output/endoscope_mast3r_finetune_nofreeze \
+--data-name servct \
+--model-name checkpoints/together_unfreeze_head_decoder_0510/checkpoint-best.pth
--- zeroshot/inference_fundus_dong.py
+++ zeroshot/inference_fundus_dong.py
@@ -53,11 +53,11 @@ def parse_args():
     # where the checkpoints is
     parser.add_argument('--base-dir', type=str, default='/data_new/luxiaoxi/code_proj/MedicalMast3R/',
                         help="project location")
-    parser.add_argument('--model-name', type=str, default='checkpoints/fundusdong_bs2_0507/checkpoint-best.pth')
+    parser.add_argument('--model-name', type=str, default='checkpoints/together_unfreeze_head_decoder_0510/checkpoint-best.pth')
     # where the endoscope is
     parser.add_argument('--input-dir', type=str, default='/data_new/luxiaoxi/dataset/medical_depth/final_version_processed')
     # parser.add_argument('--input-data', type=str, help='cutting_tissues_twice or pulling_soft_tissues', default='cutting_tissues_twice')
-    parser.add_argument('--output-dir', type=str, default='/data_new/luxiaoxi/dataset/medical_depth_output/fundus_dong/Mast3R_new_0507')
+    parser.add_argument('--output-dir', type=str, default='/data_new/luxiaoxi/dataset/medical_depth_output/fundus_dong/Mast3R_510')
     parser.add_argument('--device', type=str, default='cuda')
     # parser.add_argument('--output-dir', type=str, default='/data/luxiaoxi/dataset/eyetube_phase4_results/anterior/23_Gauge_Plaque_Dissection_of_Anterior_Persistent_Fetal_Vasculature_in_a_2_week_old_Boy/dataset0/dust3r/')
     # parser.add_argument('--model-name', type=str, default='/data/luxiaoxi/code_proj/depth_estimation/MedicalDust3R/naver/DUSt3R_ViTLarge_BaseDecoder_512_dpt.pth')
