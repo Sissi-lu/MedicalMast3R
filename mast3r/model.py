e2a440496c61b95c0767e4f3081b76716931ddb2
Sissi <670408085@qq.com>
2025-05-24 00:09:50 +0800

    git commit -m "Fintuning with unfreeze mapping only"

--- mast3r/model.py
+++ mast3r/model.py
@@ -11,6 +11,10 @@ import os
 from mast3r.catmlp_dpt_head import mast3r_head_factory
 
 import mast3r.utils.path_to_dust3r  # noqa
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from dust3r.model import AsymmetricCroCo3DStereo  # noqa
 from dust3r.utils.misc import transpose_to_landscape  # noqa
 
