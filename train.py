2d4b5db0110e32382f3fd34bcb136d12cf265db3
Sissi <670408085@qq.com>
2025-10-01 00:07:47 +0800

    git commit -m "fundus dong: finetune mast3r for half decoder and head"

--- train.py
+++ train.py
@@ -7,7 +7,7 @@
 # --------------------------------------------------------
 from mast3r.model import AsymmetricMASt3R
 from mast3r.losses import ConfMatchingLoss, MatchingLoss, APLoss, Regr3D, InfoNCE, Regr3D_ScaleShiftInv
-from mast3r.datasets import ARKitScenes, BlendedMVS, Co3d, MegaDepth, ScanNetpp, StaticThings3D, Waymo, WildRGBD, SCARED, SyntheticColon, C3VD
+from mast3r.datasets import ARKitScenes, BlendedMVS, Co3d, MegaDepth, ScanNetpp, StaticThings3D, Waymo, WildRGBD, SCARED, SyntheticColon, C3VD, FundusDong
 
 import mast3r.utils.path_to_dust3r  # noqa
 # add mast3r classes to dust3r imports
@@ -32,6 +32,7 @@ dust3r.datasets.WildRGBD = WildRGBD
 dust3r.datasets.SCARED = SCARED
 dust3r.datasets.SyntheticColon = SyntheticColon
 dust3r.datasets.C3VD = C3VD
+dust3r.datasets.FundusDong = FundusDong
 
 from dust3r.training import get_args_parser as dust3r_get_args_parser  # noqa
 from dust3r.training import train  # noqa
